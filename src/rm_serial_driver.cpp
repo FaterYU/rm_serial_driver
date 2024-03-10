dc9da561da90d4c1fd2520a98ae5d88988e87301
FaterYU <1042277145@qq.com>
2024-03-10 11:07:11 +0000

    fix: change back time offset

--- src/rm_serial_driver.cpp
+++ src/rm_serial_driver.cpp
@@ -138,9 +138,8 @@ void RMSerialDriver::receiveData() {
           geometry_msgs::msg::TransformStamped t;
           timestamp_offset_ =
               this->get_parameter("timestamp_offset").as_double();
-          // fix timestamp_offset_ to negative. 20240201-FaterYU
           t.header.stamp =
-              this->now() - rclcpp::Duration::from_seconds(timestamp_offset_);
+              this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
           t.header.frame_id = "odom";
           t.child_frame_id = "gimbal_link";
           tf2::Quaternion q;
